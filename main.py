68958f535e24e17db5c8205d3f68c8791fc2835c
HalynaYaremenko <gali.yaremenko@gmail.com>
2025-11-19 16:02:22 +0100

    added data cleansing and standardization

--- main.py
+++ main.py
@@ -1,36 +1,97 @@
 import pandas as pd
 import numpy as np
 
+pd.set_option("display.max_columns", 50) # макс к-ть стовпчиків, які ми будемо бачити у терміналі
+pd.set_option("display.width", 180)
+
 url = "https://s3-eu-west-1.amazonaws.com/shanebucket/downloads/uk-500.csv"
-df = pd.read_csv(url)
+df_origin = pd.read_csv(url)
 
 # 1. Імпорт та первинне дослідження
 
-# print(df.head())
-# print(df.info())
-# print(df.describe())
+COLUMNS_TO_DROP =[] # це константа, змінна яка змінюватись не буде
+
+print("\n------ head ------")
+print(df_origin.head())
+
+print("\n------ info ------")
+print(df_origin.info())
+
+print("\n------ describe ------")
+print(df_origin.describe())
+
+print("\n------ describe for str ------")
+print(df_origin.describe(include=[object]).T)
 
 # Перевірити якість:
 
+print('\n---- null -----')
 # print(df.isna().sum()) # пропущені
-# print(df.duplicated().sum()) # дублікати 
+print(df_origin.isna().sum().sort_values(ascending=False).head(20))
+
+print('\n---- duplicated -----')
+print(df_origin.duplicated().sum()) # дублікати шукаємо по рядках, чи не співпадають у нас повністю якісь рядки
+
+print('\n---- List columns ------')
+# list_col = df.columns
+# print(list(list_col))
+for i, col in enumerate(df_origin.columns):
+    print(f"{i:02d}. {col}")
 
 # 2. Очищення даних
 
 # Видалити непотрібні колонки (за рішенням аналітика).
 
-# Привести email та web до нижнього регістру.
+df = df_origin.copy()
 
-df['email'] = df['email'].str.lower()
-df['web'] = df['web'].str.lower()
-# print(df.head())
-
-# Очистити phone та fax від пробілів/символів.
-
-df['phone'] = df['phone1'].str.strip()
-df['fax'] = df['phone2'].str.strip()
+if COLUMNS_TO_DROP:
+    print("\n------- delete colums in list -----")
+    df = df.drop(columns=[col for col in COLUMNS_TO_DROP if col in df.columns], errors='ignore')
+else:
+    print("\nCOLUMNS_TO_DROP = []")
 
 # Стандартизувати формат текстових полів.
+
+def standardize_text(s):
+    if pd.isna(s):
+        return np.nan
+    
+    if not isinstance(s, str):
+        s = str(s)
+
+    s = s.strip()
+    s = " ".join(s.split())
+
+    return s
+
+
+possible_email_cols = [c for c in df.columns if "email" in c.lower()]
+possible_web_cols = [c for c in df.columns if ("web" in c.lower() or "website" in c.lower() or "url" in c.lower())]
+possible_phone_cols = [c for c in df.columns if ("phone" in c.lower() or "telephone" in c.lower() or "tel" in c.lower())]
+possible_fax_cols = [c for c in df.columns if "fax" in c.lower()]
+
+print("\nPossible columns: ")
+print("Email columns: ", possible_email_cols)
+print("Web columns: ", possible_web_cols)
+print("Phone columns: ", possible_phone_cols)
+print("Fax columns: ",possible_fax_cols)
+
+# Приміняємо зміни
+
+for col in df.select_dtypes(include=['object']).columns:
+    df[col] = df[col].apply(standardize_text)
+
+# Привести email та web до нижнього регістру.
+
+# email
+for col in possible_email_cols:
+    df[col] = df[col].str.lower()
+
+# web
+for col in possible_web_cols:
+    df[col] = df[col].str.lower()
+
+# Очистити phone та fax від пробілів/символів.
 
 # 3. Створення нових колонок (Feature Engineering)
 # Нова колонка	Опис
